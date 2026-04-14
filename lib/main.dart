1aeedaf27da727b20fbbbe6f3e165335416bc15e
htoopyaelinn56 <hplinn0@gmail.com>
2026-04-14 11:20:29 +0630

    Add ProviderScope to main app and update pubspec.yaml for flutter_riverpod dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'pages/login_page.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
