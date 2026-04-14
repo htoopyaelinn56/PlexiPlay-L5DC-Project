11931d9996e475f2d703ab6de26bf785857410f7
htoopyaelinn56 <hplinn0@gmail.com>
2026-04-14 11:13:21 +0630

    Refactor NeoTextField to StatefulWidget and add visibility toggle for obscure text

--- lib/widgets/neo_text_field.dart
+++ lib/widgets/neo_text_field.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import '../theme/neo_theme.dart';
 
-class NeoTextField extends StatelessWidget {
+class NeoTextField extends StatefulWidget {
   final String labelText;
   final String hintText;
   final bool obscureText;
@@ -16,12 +16,25 @@ class NeoTextField extends StatelessWidget {
   });
 
   @override
+  State<NeoTextField> createState() => _NeoTextFieldState();
+}
+
+class _NeoTextFieldState extends State<NeoTextField> {
+  late bool _isObscured;
+
+  @override
+  void initState() {
+    super.initState();
+    _isObscured = widget.obscureText;
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
-          labelText.toUpperCase(),
+          widget.labelText.toUpperCase(),
           style: const TextStyle(fontWeight: FontWeight.w700, fontSize: 14),
         ),
         const SizedBox(height: 6),
@@ -34,15 +47,30 @@ class NeoTextField extends StatelessWidget {
             onTapOutside: (_) {
               FocusScope.of(context).unfocus();
             },
-            controller: controller,
-            obscureText: obscureText,
+            controller: widget.controller,
+            obscureText: _isObscured,
             style: const TextStyle(fontWeight: FontWeight.w600, fontSize: 16),
             decoration: InputDecoration(
-              hintText: hintText,
+              hintText: widget.hintText,
               hintStyle: TextStyle(
                 color: NeoTheme.black.withOpacity(0.4),
                 fontWeight: FontWeight.w500,
               ),
+              suffixIcon: widget.obscureText
+                  ? IconButton(
+                      icon: Icon(
+                        _isObscured
+                            ? Icons.visibility_off_rounded
+                            : Icons.visibility_rounded,
+                        color: NeoTheme.black,
+                      ),
+                      onPressed: () {
+                        setState(() {
+                          _isObscured = !_isObscured;
+                        });
+                      },
+                    )
+                  : null,
               border: OutlineInputBorder(
                 borderRadius: BorderRadius.circular(NeoTheme.radiusMain),
                 borderSide: BorderSide.none,
