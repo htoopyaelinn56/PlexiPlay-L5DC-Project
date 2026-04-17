d55ce8a2528478dbe116ef73c1a2ec30d6fd5b29
htoopyaelinn56 <hplinn0@gmail.com>
2026-04-17 16:02:55 +0630

    add DownloadedVideosRepository for managing offline video storage

---
+++ lib/local_db/downloaded_videos_repository.dart
@@ -0,0 +1,47 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:isar_plus/isar_plus.dart';
+import 'package:plexi_play/local_db/downloaded_videos.dart';
+import 'package:plexi_play/local_db/isar_provider.dart';
+
+class DownloadedVideosRepository {
+  final Isar isar;
+
+  DownloadedVideosRepository({required this.isar});
+
+  Future<void> saveOfflineVideos({
+    required String filePath,
+    required String title,
+    required String thumbnailUrl,
+    required String videoUrl,
+    required String author,
+  }) async {
+    final newVideo = DownloadedVideos(
+      id: isar.downloadedVideos.autoIncrement(),
+      filePath: filePath,
+      title: title,
+      thumbnailUrl: thumbnailUrl,
+      videoUrl: videoUrl,
+      author: author,
+    );
+    await isar.writeAsync((isar) async {
+      isar.downloadedVideos.put(newVideo);
+    });
+  }
+
+  Stream<List<DownloadedVideos>> getOfflineVideos() async* {
+    final videosStream = isar.downloadedVideos.where().watch(
+      fireImmediately: true,
+    );
+    yield* videosStream;
+  }
+}
+
+final downloadedVideosRepositoryProvider = Provider((ref) {
+  final isar = ref.watch(isarProvider);
+  return DownloadedVideosRepository(isar: isar);
+});
+
+final downloadedVideosStreamProvider = StreamProvider((ref) {
+  final repository = ref.watch(downloadedVideosRepositoryProvider);
+  return repository.getOfflineVideos();
+});
